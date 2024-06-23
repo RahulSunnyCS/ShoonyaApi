ad4a6125d5ff2492bd050a377c74d8917a1230ba
Rahul Sunny <rahulscs@tekion.com>
2024-06-23 18:46:52 +0530

    Revert "added angel one helpers"
    This reverts commit 21956f4be51bb7311f53ef9150558d1b0f68bdcb.

--- helper.js
+++ helper.js
@@ -1,68 +1,37 @@
 const speakeasy = require("speakeasy");
 const Api = require("./lib/RestApi");
-let { SmartAPI, WebSocket, WebSocketV2 } = require("smartapi-javascript");
 
+api = new Api({});
 const {
   FV_USER_ID,
   FV_PASSWORD,
+  FV_TWO_FA,
   FV_API_SECRET,
   FV_VENDOR_CODE,
   FV_IMEI,
-  AO_USER_ID,
-  AO_PASSWORD,
-  AO_API_SECRET,
 } = process.env;
 const authparams = {
   userid: FV_USER_ID,
   password: FV_PASSWORD,
+  twoFA: FV_TWO_FA,
   vendor_code: FV_VENDOR_CODE,
   api_secret: FV_API_SECRET,
   imei: FV_IMEI,
 };
-api = new Api({});
-let smart_api = new SmartAPI({
-  api_key: AO_API_SECRET,
-});
+
 module.exports.loginShoonya = async () => {
   try {
     // Generate TOTP based on the secret
     const totpToken = speakeasy.totp({
-      secret: process.env.FV_TWO_FA,
+      secret: process.env.FV_SECRET,
       encoding: "base32",
     });
 
     authparams.twoFA = totpToken;
-    const response = await api.login(authparams);
+    await api.login(authparams);
     console.log("login successful to Shoonya");
-    return {
-      message: "completed the login path",
-      response,
-    }
   } catch (err) {
     console.error("login to Shoonya failed", err);
-  }
-};
-
-module.exports.loginAngelOne = async () => {
-  try {
-    // Generate TOTP based on the secret
-    const totpToken = speakeasy.totp({
-      secret: process.env.AO_TWO_FA,
-      encoding: "base32",
-    });
-    const response = await smart_api
-    .generateSession(AO_USER_ID, AO_PASSWORD, totpToken);
-    const profile = await smart_api.getProfile();
-    const history =	await smart_api.getCandleData({
-			    "exchange": "NSE",
-			    "symboltoken": "3045",
-			    "interval": "ONE_MINUTE",
-			    "fromdate": "2021-02-10 09:00",
-			    "todate": "2021-02-10 15:20"
-			})
-    return {history}
-  } catch (err) {
-    console.error("login to Angel One failed", err);
   }
 };
 
